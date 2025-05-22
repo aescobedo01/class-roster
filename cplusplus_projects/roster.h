ae3fd6010c0872b30e722675049ed39744ce7301
anaescobedo <ana.escbedo@gmail.com>
2025-05-22 17:49:19 -0500

    added const to methods in roster.h

--- cplusplus_projects/roster.h
+++ cplusplus_projects/roster.h
@@ -1,32 +1,33 @@
-#ifndef ROSTER_H
-#define ROSTER_H
+#pragma once
+
 #include "student.h"
-#include "degree.h"
 #include <string>
-#include <array>
 #include <iostream>
+#include "degree.h"
 
 using namespace std;
 
 class Roster {
 public:
 
-//student object array
+//student object array of pointers
 Student* classRosterArray[5];
-int numStudents;
+//declare variable
 int lastIndex = -1;
 
-Roster(int numStudents);
+//default constructor and destructor
+Roster();
 ~Roster();
 
+//declare methods to print all info
+//assign parameters to mmeber variables
 string GetId(int index);
 void parse (string const studentDataString);
 bool remove(string studentId);
 void add(string studentId, string firstName, string lastName, string emailAddress, int yearsOld, int daysCourse1, int daysCourse2, int daysCourse3, Degree degreeProgram);
-void printAll();
-void printAverageDaysInCourse(string studentId);
-void printInvalidEmails();
-void printByDegreeProgram(Degree degreeProgram);
+void printAll() const;
+void printAverageDaysInCourse(string studentId) const;
+void printInvalidEmails() const;
+void printByDegreeProgram(Degree degreeProgram) const;
 
 };
-#endif
